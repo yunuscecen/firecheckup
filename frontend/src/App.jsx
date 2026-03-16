6660d5b070872fff1a8297660092ce29acdfcb93
yunuscecen <yunusccn@outlook.com>
2026-03-16 20:03:32 +0300

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PublicSearch from './pages/PublicSearch';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import AdminDashboard from './pages/AdminDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        {/* Herkese açık sorgulama ekranı ana sayfa olacak */}
         <Route path="/" element={<PublicSearch />} />
+        <Route path="/login" element={<Login />} />
         
-        {/* İleride ekleyeceğimiz diğer rotalar buraya gelecek */}
-        {/* <Route path="/login" element={<Login />} /> */}
+        {/* Otel Paneli */}
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } 
+        />
+
+        {/* Admin Paneli */}
+        <Route 
+          path="/admin" 
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          } 
+        />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
